a4c0607902e3695a079b5918641ed02ae1ca59d7
Shane1993 <513199010@qq.com>
2015-06-23 00:20:40 +0800

    ...

--- Paint01_BasicUseOfPaintingAPI/app/src/main/java/net/lzs/paint01_basicuseofpaintingapi/MainActivity.java
+++ Paint01_BasicUseOfPaintingAPI/app/src/main/java/net/lzs/paint01_basicuseofpaintingapi/MainActivity.java
@@ -2,6 +2,7 @@ package net.lzs.paint01_basicuseofpaintingapi;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -14,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
     private MyPoint mp;
     private RelativeLayout rl;
     private int max_x,max_y;
+    private float input_x,input_y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +31,22 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View view) {
 
-
-                if(max_x == 0 && max_y == 0) {
+                if (max_x == 0 && max_y == 0) {
                     max_x = rl.getWidth();
                     max_y = rl.getHeight();
                     System.out.println(max_x + "-----------------------------" + max_y);
                 }
 
-                mp.setX(Float.parseFloat(et_x.getText().toString()) * max_x / 100.0f);
-                mp.setY(Float.parseFloat(et_y.getText().toString()) * max_y / 100.0f);
+                if (!TextUtils.isEmpty(et_x.getText().toString()) && !TextUtils.isEmpty(et_y.getText().toString())) {
 
+                    input_x = Float.parseFloat(et_x.getText().toString());
+                    input_y = Float.parseFloat(et_y.getText().toString());
+
+                    if (input_x >= 0.0 && input_x <= 100.0 && input_y >= 0.0 && input_y <= 100) {
+                        mp.setX(input_x * max_x / 100.0f);
+                        mp.setY(input_y * max_y / 100.0f);
+                    }
+                }
             }
         });
     }
